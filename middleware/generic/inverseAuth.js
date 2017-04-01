8bc3947da519e077522b9c5bf2d0678aad2bbcf1
Balázs Rostás <rostas.balazs@gmail.com>
2017-04-01 10:12:29 +0200

    deleted cached

--- middleware/generic/inverseAuth.js
+++
@@ -1,9 +0,0 @@
-/**
- * If the user is logged in, redirects to /
- */
-module.exports = function (objectRepository) {
-  return function (req, res, next) {
-    return next();
-  };
-
-};
--- middleware/generic/render.js
+++
@@ -1,9 +0,0 @@
-/**
- * Using the template engine render the values into the template
- */
-module.exports = function (objectRepository, viewName) {
-  return function (req, res) {
-    res.end('Render: ' + viewName);
-  };
-
-};
